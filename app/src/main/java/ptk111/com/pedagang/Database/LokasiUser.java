33f68aacc86195104f743a42eacf127ead3bd71f
ptk111 <kampakhitam_berkarat@yahoo.com>
2017-05-16 21:27:47 +0700

    v1.1

---
+++ app/src/main/java/ptk111/com/pedagang/Database/LokasiUser.java
@@ -0,0 +1,31 @@
+package ptk111.com.pedagang.Database;
+
+/**
+ * Created by ptk111 on 5/8/2017.
+ */
+
+public class LokasiUser extends Koneksi {
+    String URL = "http://agungcahya.esy.es/server.php";
+    String url = "";
+    String response = "";
+
+    public String tampilLokasiPembeli() {
+        try {
+            url = URL + "?operasi=viewPembeli";
+            System.out.println("URL Tampil Lokasi Pembeli: " + url);
+            response = call(url);
+        } catch (Exception e) {
+        }
+        return response;
+    }
+
+    public String updateLokasiPedagangById(Integer id, Double latitude, Double longitude) {
+        try {
+            url = URL + "?operasi=updatePedagangById&id=" + id + "&latitude=" + latitude + "&longitude=" + longitude;
+            System.out.println("URL Update Lokasi Pedagang By ID : " + url);
+            response = call(url);
+        } catch (Exception e) {
+        }
+        return response;
+    }
+}
